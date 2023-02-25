f8cf80db89a411843d65a50823a51c97a0a0ceae
Andrey <104921155+AndreyPerunov@users.noreply.github.com>
2023-02-25 19:15:39 +0200

    Changed layout added theater feature

--- app/Main.js
+++ app/Main.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import ReactDOM from "react-dom/client"
 
 import "./styles/main.scss"
@@ -8,18 +8,39 @@ import VideoPlayer from "./components/VideoPlayer"
 import Cards from "./components/Cards"
 
 function Main() {
+  const [theaterMode, setTheaterMode] = useState(false)
+
+  const handleTheaterModeToggle = () => {
+    console.log(theaterMode)
+    setTheaterMode(!theaterMode)
+  }
+
+  const checkKeyPress = useCallback(
+    e => {
+      const { keyCode } = e
+      if (keyCode === 84) {
+        setTheaterMode(!theaterMode)
+      }
+    },
+    [theaterMode]
+  )
+
+  useEffect(() => {
+    document.addEventListener("keydown", checkKeyPress)
+    return () => document.addEventListener("keydown", checkKeyPress)
+  }, [checkKeyPress])
+
   return (
     <>
-      <div className="theater">{/* <VideoPlayer /> */}</div>
-      <div className="wrapper">
-        <div className="row row--gutters">
-          <div className="row__large-9">
-            <VideoPlayer />
-            <Cards amount="2" modifier="green" />
-          </div>
-          <div className="row__large-3">
-            <Cards amount="8" />
-          </div>
+      <div className={`${theaterMode ? "container-theater-mode" : "container"}`}>
+        <div className="video">
+          <VideoPlayer toggleThreaterMode={handleTheaterModeToggle} />
+        </div>
+        <div className="Card">
+          <Cards amount="12" modifier="green" />
+        </div>
+        <div className="RightCard">
+          <Cards amount="8" />
         </div>
       </div>
     </>
